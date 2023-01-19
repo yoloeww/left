2737ee025b12b31edf3fdc0e1e024e209334f9e5
yolo <77111763+yoloeww@users.noreply.github.com>
2023-01-19 22:03:25 +0800

    Update 3.堆排.cpp

--- 3.堆排.cpp
+++ 3.堆排.cpp
@@ -63,4 +63,70 @@ void heapSort(vector<int> arr) {
 }
 ```
 
+# 二、计数排序
 
+  > - 计数排序是不基于比较的排序 
+   >-  不基于比较的排序，都是根据具体的数据状况做的排序，应用范围不广，很窄的一路数据排序算法。
+   > - 假设一个整数数组，里面的数都是员工的年龄 
+   >  - 分析： 由于员工的年龄是18-60岁之间，所以我们假设数据的范围为0-100
+   > - 我们申请一个长度为100的数组，我们规定在数组的第18位置上的数为18岁的人有多少个（频数）。 即下标为年龄，位置上的数为该年龄的数量。
+   > - 该算法的时间复杂度为O(N)
+   
+## 1、思路
+
+ - 先准备一个有限个数（比如200）的整型辅助数组，挨个遍历原始数组，得到的值是 i，则将辅助数组 i 位置的值加一
+ - 遍历辅助数组，如果数组位置 i 上的值是k，则输出k个i，得到的即是排好序的数组
+
+```cpp
+
+   void countSort(vector<int> arr){
+      if(arr.size() < 2)
+      return;
+      int maxt = Integer.MIN_VALUE;
+      for(int v : arr) {
+         maxt = max(maxt, v);
+ 	  }
+ 	  // 统计每个数出现的个数
+ 	  vector<int> nums(maxt + 1);
+ 	  for(int v : arr) {
+ 	  	nums[v]++;
+ 	  }
+ 	  // 从小到大开始排
+ 	  int i = 0;
+ 	  for (int j = 0; j < nums.size(); j++) {
+ 	     while (nums[j] -- > 0)
+ 	     arr[i++] = j;
+ 	  }
+	}
+  
+```
+# 三、基数排序
+>适用于十进制正整数
+
+ 1. 遍历整个数组，得到最大值的十进制位数，同时将不满足最大位数的数高位补0。
+ 2. 准备十个桶，从0 - 9编号，每个桶大小为原数组大小。
+3. 遍历数组，以个位数为标准，个位数的值是 i，则将其放到第 i 号桶里；遍历完后，从0号桶开始，挨个将里面的数倒出来（先进先出）。此时的数是根据个位数排序。
+4. 遍历数组，以十位数为标准，十位数的值是 i，则将其放到第 i 号桶里；遍历完后，从0号桶开始，挨个将里面的数倒出来（先进先出），此时的数是根据十位数排序。
+
+```cpp
+ void radixSort(vector<int> arr){
+      if(arr.size() < 2)
+      return;
+      sort(arr, 0, arr.size() - 1, maxBit(arr));
+      }
+ void maxBit(vector<int> arr){    
+ 	int maxt = Integer.MIN_VALUE;
+    for (int value : arr) {
+        maxt = max(maxt, value);
+    }
+    int res = 0;
+    while (maxt != 0) {
+        res++;
+        maxt /= 10;
+    }
+    return res;
+ }
+void sort(vector<int> arr, int l, int r, int digit) {
+	int radix = 10;
+}
+```
